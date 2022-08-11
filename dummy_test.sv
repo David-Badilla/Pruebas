71e37aa831055fe6206d259e876a80abf2711fcb
David-Badilla <davidbadilla@estudiantec.cr>
2022-08-10 22:34:53 -0600

    Comentarios

--- dummy_test.sv
+++ dummy_test.sv
@@ -32,7 +32,7 @@ module Sim_fifo;
 	
     int clock_counter;
     
-	int Cerofin;
+	int Cerofin;  // variables de control se activa una a una cada vez que finaliza una prueba
     int Primfin;
     int Segfin;
     int Terfin;
@@ -45,9 +45,9 @@ initial begin
         push =0;
         pop = 0;
         Din=0;
-        clock_counter =0;
+        clock_counter=0;
         
-        Cerofin=1;
+        Cerofin=1; //variables de control se activa una a una cada vez que finaliza una prueba
 		Primfin=0;
 		Segfin=0;
 		Terfin=0;
@@ -56,7 +56,7 @@ end
 
 always #1 clk=~clk;   
 always@(posedge clk)begin
-  if(clock_counter > 4) begin
+  if(clock_counter > 4) begin	//Verifica si ya pasaron los 4 ciclos de reinicio
     rst = 0;
 	if (Cerofin==1)begin //Revisa variables de control que cabian progresivamente
     	prueba(); // LLama a una funcion prueba
@@ -81,7 +81,7 @@ end
  int ciclo =0;
  int dato =0;
  //--------------PRUEBA LLENADO Y VACIADO-------------------------------------------------------
-  task prueba();		//funcion prueba
+  task prueba();		//funcion prueba Profe
 
       if(full==1)begin // Cambia si la fifo está llena
         ciclo=1;
@@ -107,18 +107,18 @@ end
       if(pop==1)begin
         $display("at %g poped data: %g count: %g",$time,Dout,Sim_fifo.uut.count);
       end
-      if(pndng == 0)begin
+      if(pndng == 0)begin //La prueba actual finaliza
 		
-		Cerofin=0;//Cambio de variables de control
+		Cerofin=0;//Cambio de variables de control para activar la siguiente 
       	Primfin=1;
       	
-      	dato=0;
+      	dato=0;	//Reinicio de variables importantes en las siguientes pruebas
       	ciclo=0;
       	
       	
       	rst=1;	//Cambio para aplicar reset despues de acabar esta prueba
       	clock_counter=0; //reinicio del contador para aplicar reset por 4 ciclos
-        $display("-------PRUEBA OVERFLOW------40 datos seguidos-------------------------------");
+        $display("-------PRUEBA OVERFLOW------40 datos seguidos-------------------------------"); //Imprime en terminal la siguiente prueba a realizar - la coloqué aqui para que no se repitiera en cada ciclo
       end
     end
     default:begin
@@ -141,20 +141,21 @@ end
 	  end
   	  		
 	  if(dato==40)begin // para detener la prueba luego de 40 datos
-	  	Primfin=0;  		
+	  	Primfin=0;  //Variables de control de siguiente prueba
 		Segfin=1;
+		
 		dato=0; //Reinicio variable dato
 		pop=0; //por fallos en el inicio de la siguiente prueba
 		
 		rst=1; //Variables para aplicar reset
 		clock_counter=0;
-		$display("-------PRUEBA UNDERFLOW---------20 veces pop---------------------------");
+		$display("-------PRUEBA UNDERFLOW---------20 veces pop---------------------------");//Imprime el titulo de la siguiente prueba 
 	  end			
   
   endtask
   
   //------------------PRUEBA UNDERFLOW---------------------------------------------------
-  task underflow();
+  task underflow(); 
   	rst = 0;
     push = 0;
     pop=~pop;
@@ -164,7 +165,7 @@ end
         dato=dato+1;
       end
   		
-  	if (dato==20)begin //detiene la prueba luego de introducir 20 datos
+  	if (dato==20)begin //detiene la prueba luego de introducir 20 datos solo porque si
   		
   		Segfin=0;// variables de control para iniciar la siguiente prueba
 		Terfin=1;
@@ -176,16 +177,16 @@ end
 		
 		rst=1;	//Para aplicar un reset al finalizar esta prueba
 		clock_counter=0;	//reinicia contador para aplicar reset por 4 ciclos
-		$display("-------PRUEBA PUSH POP-------------------------------------");
+		$display("-------PRUEBA PUSH POP-------------------------------------"); //Imprime el siguiente titulo
 	end
 	
   endtask
   
- //--------------------PRUEBA PUSH POP-------------------------------------------------
+ //--------------------PRUEBA PUSH POP al mismo tiempo-------------------------------------------------
   task pushpop();
   	rst = 0;
   	push = ~push;
-  	pop=~pop;
+  	pop =~ pop;
   	
   	if(push==1 && pop==1)begin
 	    $display("at %g pushed data: %g poped data: %g count %g",$time,dato,Dout,Sim_fifo.uut.count);
@@ -198,11 +199,13 @@ end
  
       
       
-     if(dato == 17)begin
+     if(dato == 17)begin	//Para detener la prueba luego de 17 datos
 		Terfin=0;	//Variables de control para continuar con la siguiente prueba
 		Cuarfin=1;
+		
 		ciclo=0;
 		dato=0;
+		
 		rst=1;		//Aplicar el reset antes de iniciar la siguiente prueba
 		clock_counter=0;	//Reiniciar el contador para aplicar reset por 4 ciclos
 		$display("-------PRUEBA PUSH POP INTERCALADO-------------------------");
@@ -215,12 +218,13 @@ end
  //--------------------PRUEBA PUSH POP INTERCALADO-------------------------------------------------
   task pushpopinter();
   
-  if(dato == 17)begin
+  if(dato == 17)begin //Detiene la prueba luego de n datos escritos en push
     $finish;
         
   end
+  
   case(ciclo)
-    0: begin 		// ciclo de llenado de fifo
+    0: begin 		// ciclo de llenado de push
       rst = 0;
       push = ~push;
       pop=0;
@@ -229,17 +233,17 @@ end
         $display("at %g pushed data: %g count %g",$time,dato,Sim_fifo.uut.count);
       end else begin
         dato=dato+1;
-        ciclo=~ciclo;
+        ciclo=~ciclo;		//Cambia la variable ciclo para que en el siguiente ciclo sea de pop
       end
     end
-    1: begin		//ciclo de vaciado 
+    1: begin		//ciclo de pop
       rst = 0;
       push = 0;
       pop=~pop;
       Din = dato;
       if(pop==1)begin
         $display("at %g poped data: %g count: %g",$time,Dout,Sim_fifo.uut.count);
-        ciclo=~ciclo;
+        ciclo=~ciclo;		//Cambia la variable ciclo para que en el siguiente ciclo sea de push
       end
     end
     
