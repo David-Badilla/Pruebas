095be2e428e371be3d9b4559153d413cc6d8a483
David-Badilla <davidbadilla@estudiantec.cr>
2022-08-10 18:38:08 -0600

    Intentos de pruebas

--- dummy_test.sv
+++ dummy_test.sv
@@ -111,7 +111,7 @@ end
 		Cerofin=0;
       	Primfin=1;
       	dato=0;
-        $display("-------PRUEBA OVERFLOW-------------------------------------");
+        $display("-------PRUEBA OVERFLOW------40 datos seguidos-------------------------------");
       end
     end
     default:begin
@@ -124,59 +124,70 @@ end
   task overflow();
 
 
-  case(ciclo)
-    0: begin 		// ciclo de llenado de fifo
-      rst = 0;
-      push = ~push;
-      pop=0;
-      Din = dato;
-      if(push==1)begin
-        $display("at %g pushed data: %g count %g Salida %g",$time,dato,Sim_fifo.uut.count,Dout);
-      end else begin
-        dato=dato+1;
-      end
-    end
-  		
-  		
-  	  		
-  if(dato==40)begin // Cambia si la fifo está llena
-        
-        
-	  $display("-------PRUEBA UNDERFLOW------------------------------------");
-			Primfin=0;  		
-			Segfin=1;
-		    
-        
-        
-      end	
-  		
-  		
-  		
-  		
-  		
-  		
-
-		
+	  case(ciclo)
+		0: begin 		// ciclo de llenado de fifo
+		  rst = 0;
+		  push = ~push;
+		  pop=0;
+		  Din = dato;
+		  if(push==1)begin
+		    $display("at %g pushed data: %g count %g Salida %g",$time,dato,Sim_fifo.uut.count,Dout);
+		  end else begin
+		    dato=dato+1;
+		  end
+		end
+	  		
+	  		
+	  	  		
+	  if(dato==40)begin // para detener la prueba
+	  	Primfin=0;  		
+		Segfin=1;
+		dato=0; //Reinicio variable dato
+		$display("-------PRUEBA UNDERFLOW---------20 veces pop---------------------------");
+	  end			
   
   endtask
   
   //------------------PRUEBA UNDERFLOW---------------------------------------------------
   task underflow();
+  	rst = 0;
+    push = 0;
+    pop=~pop;
+    Din = dato;
+    if(pop==1)begin
+        $display("at %g poped data: %g count: %g",$time,Dout,Sim_fifo.uut.count);
+        dato=dato+1;
+      end
   		
-  		
-  		
-  		
-		Segfin=0;
-  		Terfin=1;
+  	if (dato==20)begin
+  		Segfin=0;
+		Terfin=1;
+		dato=0;
 		$display("-------PRUEBA PUSH POP-------------------------------------");
+	end
+	
   endtask
   
  //--------------------PRUEBA PUSH POP-------------------------------------------------
   task pushpop();
-  		
+  	rst = 0;
+  	push = ~push;
+  	pop=~pop;
+  	if(push==1 & pop==1)begin
+        $display("at %g pushed data: %g oped data: %g count %g",$time,dato,Dout,Sim_fifo.uut.count);
+      end else begin
+      	dato=dato+1;
+      end
+      
+     if(pndng == 0)begin
 		Terfin=0;
 		Cuarfin=1;
 		$display("-------PRUEBA PUSH POP INTERCALADO-------------------------");
+	  end
+  	
+  	
+  		
+		
   endtask
  //--------------------PRUEBA PUSH POP INTERCALADO-------------------------------------------------
   task pushpopinter();
